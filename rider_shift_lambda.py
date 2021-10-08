b2b16d98655cd7038792d32c874bbd8cb0c497af
abdulrehmanase <abdul.rehman.ase@cheetay.pk>
2021-10-08 12:01:56 +0500

    "second commit"

--- rider_shift_lambda.py
+++ rider_shift_lambda.py
@@ -1,35 +1,101 @@
 from utils import *
-from sql import rider_shifts_sql
+from sql import *
 
-ID = 'ID'
-SHIFT_START_DATE = 'Shift Start Date'
-SHIFT_START_TIME = 'Shift Start Time'
-SHIFT_END_DATE = 'Shift End Date'
-SHIFT_END_TIME = 'Shift End Time'
-HOT_SPOT = 'HotSpot'
+NIC = 'CNIC'
 CITY = 'City'
-RIDER_REQUIRED = 'Rider Required'
+RIDER_CATEGORY = "Rider Category"
+RIDER_TYPE = "Rider Type"
+TOTAL_PICKED_UP_ORDERS = 'Total Orders(Failed + Delivered)'
+WEEKEND_ORDERS = 'Weekend Total Orders'
+HOURS_WORKED = 'Sum of Hours worked(Worked hours - Pause time)'
+UTR = 'UTR'
+PICKUP_PAY = 'Pickup PKR (PP)'
+DROP_OFF_DISTANCE_PAY = 'Total Drop-off Pay PKR (DDP)'
+DROP_OFF_DISTANCE = "Drop-off Distance KM"
+PICKUP_BONUS = 'Total Pickup bonus (PKR) (PB)'
+PICKUP_DISTANCE = "Pickup Distance KM"
+DROP_OFF_PAY = 'Total Drop-off pay (DP)'
+# DELIVERY_CHARGES_BASED_PAY_PAY = RiderEarningsLogType.delivery_charges_based_pay.label
+# PER_ORDER_PAY = RiderEarningsLogType.per_order_pay.label
+# SLAB_BASED_PAY = RiderEarningsLogType.slab_based_pay.label
+# LATE_NIGHT_BONUS = RiderEarningsLogType.late_night_bonus_pay.label
+NO_SHOW_DAYS = 'No show days (only the days when at least one shift was scheduled)'
+PENALTY = 'Penalty'
+TOTAL_WITHOUT_GUARANTEE = 'Total without guarantee'
+PER_HOUR_INCOME = 'Per hour income'
+GUARANTEED_PAY_PER_HOUR = 'Per hour income on the basis of weighted average of hours'
+ACCEPTANCE_RATE = 'Acceptance %'
+FAILED_ORDERS = 'Failed orders'
+UNACCEPTED_ORDERS = 'UnAccepted orders'
+FAILED_RATE = 'Failed Order Percentage'
+APP_ON_RATE = 'App on-time % (cumulative)'
+GUARANTEE_QUALIFIES = 'Guarantee Qualifies'
+GUARANTEE_RATE = 'Guarantee rate'
+FOOD_ORDER_PAY = 'Food Order Pay'
+HEALTH_CARE_ORDER_PAY = 'HealthCare Order Pay'
+ERRAND_ORDER_PAY = "Errand Order Pay"
+BOOKS_ORDER_PAY = 'Books Order Pay'
+BEAUTY_ORDER_PAY = 'Beauty Order Pay'
+BABY_CARE_ORDER_PAY = 'BabyCare Order Pay'
+PANTRY_ORDER_PAY = 'Pantry Order Pay'
+PHARMA_ORDER_PAY = 'Pharma Order Pay'
+TIFFIN_ORDER_PAY = 'Tiffin Order Pay'
+XOOM_ORDER_PAY = 'Xoom Order Pay'
+TOTAL_PAY_WITH_GUARANTEE = 'Total pay with guarantee'
+SIGN_UP_BONUS = 'Sign Up Bonus'
+LAST_SALARY_DIFFERENCE = 'Last Salary Difference'
+LOYALTY_BONUS_REDEEMED = 'Loyalty bonus redeemed'
+OVER_TIME_PAY = 'Over Time Pay'
+FINAL_PAYOUT = 'Final Payout'
+FUEL_ALLOWANCE = "Fuel Allowance"
+ON_TIME_RATE = 'One Time Rate'
+TOTAL_ON_TIME_DELIVERIES = 'Number of on time drop offs'
+TOTAL_ON_TIME_PICK_UPS = 'Number of on time pickups'
+REFERRAL_BONUS = 'Referral Bonus'
+CERTIFICATE_BONUS = 'Certificate Bonus'
+SECURITY_DEPOSITS_DEDUCTION = "Security Deposits Deduction"
+RIDER_WALLET = "Rider Wallet"
 
 
-def rider_fill_rate(start_date, end_date):
+def rider_salary(start_date, end_date):
     connection = connect_to_db()
     cursor = connection.cursor()
-    cursor.execute(rider_shifts_sql(start_date, end_date))
+    cursor.execute(get_data(start_date, end_date))
 
-    shifts = cursor.fetchall()
+    riders = cursor.fetchall()
 
-    riders_data = [{ID: shift[0], SHIFT_START_DATE:shift[1], SHIFT_START_TIME: shift[2], SHIFT_END_DATE:shift[3],
-                    SHIFT_END_TIME:shift[4], HOT_SPOT:shift[5], CITY: shift[6], RIDER_REQUIRED:shift[7]}
-                   for shift in shifts]
+    for rider in riders:
+        rider_id = rider[0]
+        pick_up_distance = get_rider_pickup_distances(rider=rider[0], start_time=start_date,
+                                                                   end_time=end_date, log_type="PB")
+        drop_off_distance = get_rider_drop_off_distances(rider=rider[0], start_time=start_date,
+                                                                   end_time=end_date, log_type="DDP")
+        rider_earning = get_rider_earnings(rider=rider[0], start_time=start_date,
+                                                                   end_time=end_date)
+        pick_up_distance_bonus = rider_earning[0][0] or 0
+        pick_up_pay = rider_earning[0][1] or 0
+        drop_off_distance_pay = rider_earning[0][2] or 0
+        drop_off_pay = rider_earning[0][3] or 0
+        delivery_charges_based_pay = rider_earning[0][4] or 0
+        per_order_pay = rider_earning[0][5] or 0
+        slab_based_pay = rider_earning[0][6] or 0
+        tip_pay = rider_earning[0][7] or 0
+        total_late_night_bonus_pay = rider_earning[0][12] or 0
 
-    header = [ID, SHIFT_START_DATE, SHIFT_START_TIME, SHIFT_END_DATE, SHIFT_END_TIME, HOT_SPOT, CITY, RIDER_REQUIRED]
+        rider_earning_category=get_rider_earnings_by_category(rider=rider[0], start_time=start_date,
+                                                                   end_time=end_date)
 
-    file_name = 'Rider Fill Rate.csv'
-    zip_file = create_csv(file_name, riders_data, header)
-    attachments = [{'name': file_name + '.zip', 'content': zip_file.getvalue()}]
-    title = 'Rider Fill Report'
-    print(zip_file)
+        food_order_pay = rider_earning_category[0][0] or 0
+        healthcare_order_pay = rider_earning_category[0][1] or 0
+        errand_pay = rider_earning_category[0][2] or 0
+        books_order_pay = rider_earning_category[0][3] or 0
+        beauty_order_pay = rider_earning_category[0][4] or 0
+        babycare_order_pay = rider_earning_category[0][5] or 0
+        pantry_order_pay = rider_earning_category[0][6] or 0
+        pharma_order_pay = rider_earning_category[0][7] or 0
+        tiffin_order_pay = rider_earning_category[0][8] or 0
+        xoom_order_pay = rider_earning_category[0][9] or 0
 
 
-rider_fill_rate("2019-10-10", "2020-10-10")
+rider_salary("2021-05-09 23:59:00", "2021-10-07 07:18:18.754045 ")
 
--- sql.py
+++ sql.py
@@ -1,11 +1,100 @@
+from utils import *
 
-def rider_shifts_sql(start_date, end_date):
-    sql_query = (""" SELECT shift.id AS ID ,DATE_FORMAT(shift.start_at , '%Y-%m-%d') ,
-                    DATE_FORMAT(shift.start_at ,'%H:%i:%s') , DATE_FORMAT(shift.close_at , '%Y-%m-%d') ,
-                    DATE_FORMAT(shift.close_at ,'%H:%i:%s') ,h.area_name AS HOT_SPOT,c.name AS city ,shift.riders_required AS RIDER_REQUIRED FROM
-                    (shift
-                     INNER JOIN  hotspot h
-                     ON shift.hotspot_id = h.id)
-                     LEFT JOIN city c ON h.city_id = c.id where shift.start_at< '{}' and shift.close_at>'{}'""".format(
-        end_date, start_date))
+
+def get_data(start_date, end_date):
+    sql_query = ("""select rider.id  from rider INNER JOIN city c on rider.city_id = c.id where 
+                    (rider.city_id is not NULL AND rider.id IN (SELECT rs.rider_id from rider_shift rs 
+                    inner join shift s on rs.shift_id = s.id where s.start_at  BETWEEN  '{}' AND '{}' ) )  limit 10 """.format(start_date,end_date))
     return sql_query
+
+
+def get_rider_pickup_distances(rider, start_time, end_time, log_type):
+    pick_up_distance_sql = ("""select  sum(od.pickup_distance) from `order`o right join rider_earnings re 
+                    on o.id=re.order_id
+                    inner join order_distance od 
+                    on od.order_id = o.id  
+                    WHERE (re.created_at BETWEEN '{}' AND 
+                    '{}') AND 
+                    re.log_type ='{}' and
+                    re.rider_id='{}'
+                    """.format(start_time, end_time, log_type, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(pick_up_distance_sql)
+    pick_up_distance = cursor.fetchall()
+    if pick_up_distance[0][0]:
+        return pick_up_distance[0][0]
+    return 0
+
+
+def get_rider_drop_off_distances(rider, start_time, end_time, log_type):
+
+    drop_off_distance_sql = (""" select sum(od.delivered_distance) from `order`o right join rider_earnings re 
+                                on o.id=re.order_id
+                                inner join order_distance od 
+                                on od.order_id = o.id  
+                                WHERE (re.created_at BETWEEN '{}' AND 
+                                '{}') AND 
+                                re.log_type ='{}' and
+                                re.rider_id='{}'
+                                 """.format(start_time, end_time, log_type, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(drop_off_distance_sql)
+    drop_off_distance = cursor.fetchall()
+    if drop_off_distance[0][0]:
+        return drop_off_distance[0][0]
+    return 0
+
+
+def get_rider_earnings(rider, start_time, end_time):
+
+    rider_earnings_sql = ("""SELECT  
+                        SUM(CASE when re.log_type="PB" then amount  END) as pick_up_distance_bonus ,
+                        SUM(CASE when re.log_type="PP" then amount  END) as pick_up_pay ,
+                        SUM(CASE when re.log_type="DDP" then amount  END) as drop_off_distance_pay,
+                        SUM(CASE when re.log_type="DP" then amount  END) as drop_off_pay,
+                        SUM(CASE when re.log_type="DCP" then amount  END) as delivery_charges_based_pay,
+                        SUM(CASE when re.log_type="FP" then amount  END) as per_order_pay,
+                        SUM(CASE when re.log_type="SBP" then amount  END) as slab_based_pay,
+                        SUM(CASE when re.log_type="TP" then amount  END) as tip_pay,
+                        
+                        COUNT(case when re.log_type="PP" then order_id  END) total_pick_ups,
+                        COUNT(case when re.log_type="DP" then order_id  END) total_drop_offs,
+                        COUNT(case when re.log_type="FP" then order_id  END) total_per_order_pays,
+                        COUNT(case when re.log_type="SBP" then order_id  END) total_slab_based_pays,
+                        SUM(CASE when re.log_type="LNB" then amount  END) as total_late_night_bonus_pay
+                        from rider_earnings re  where re.created_at BETWEEN '{}'
+                        AND '{}'
+                        AND re.rider_id ='{}' 
+                        """.format(start_time, end_time, rider))
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(rider_earnings_sql)
+    drop_off_distance = cursor.fetchall()
+    return drop_off_distance
+
+
+def get_rider_earnings_by_category(rider, start_time, end_time):
+    get_rider_earnings_by_category_sql=("""select 
+                                    SUM(CASE when o.category="food" then amount  END) as food,
+                                    SUM(CASE when o.category="healthcare" then amount  END) as healthcare,
+                                    SUM(CASE when o.category="errand" then amount END) as errand,
+                                    SUM(CASE when o.category="books" then amount  END) as books,
+                                    SUM(CASE when o.category="beauty" then amount  END) as beauty,
+                                    SUM(CASE when o.category="babycare" then amount  END) as babycare,
+                                    SUM(CASE when o.category="pantry" then amount  END) as pantry,
+                                    SUM(CASE when o.category="pharma" then amount  END) as pharma,
+                                    SUM(CASE when o.category="tiffin" then amount  END) as tiffin,
+                                    SUM(CASE when o.category="xoom" then amount  END) as xoom
+                                    from  rider_earnings re 
+                                    right join `order` o on re.order_id =  o.id 
+                                    WHERE  re.created_at BETWEEN '{}'
+                                    AND '{}'
+                                    AND re.rider_id ='{}' """.format(start_time,end_time,rider))
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(get_rider_earnings_by_category_sql)
+    rider_earning_by_category = cursor.fetchall()
+    return rider_earning_by_category
